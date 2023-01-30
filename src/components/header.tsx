f1565ee3c7437e48ed03d3fbb2a779557b5e802c
Alexey Boyko <a.boyko@anviovr.com>
2023-01-31 01:49:27 +0300

    init commit

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type HeaderType = {
+
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">New Feed</a>
+            <a href="">Messages</a>
+        </div>
+    )
+}
+
+export default Header;
